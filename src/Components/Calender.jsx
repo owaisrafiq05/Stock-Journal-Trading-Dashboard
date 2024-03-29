d1d23c42742933652ed2e3da5b00a16b8a054179
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-03-30 01:56:20 +0500

    calendar ui updated

--- src/Components/Calender.jsx
+++ src/Components/Calender.jsx
@@ -1,5 +1,7 @@
+// Calendar.jsx
 import React from 'react';
 import { createTheme } from '@mui/material/styles';
+import './calendar.css';
 
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -12,13 +14,21 @@ const MyCalendar = () => {
   return (
     <div className='bg-gray-800'>
     <Container maxWidth="lg">
-      <Paper elevation={3}>
+      <Paper elevation={3} sx={{backgroundColor: "rgb(31 41 55)"}}>
+        <div className='flex w-[100%] justify-between items-end'>
+            <div className='px-4 rounded-3xl bg-[#2E313F] btn'>
+              <button className='text-4xl text-white mb-2'>&lt;</button>
+            </div>
+            <div className='px-4 rounded-3xl bg-[#2E313F] btn'>
+              <button className='text-4xl text-white mb-2'>&gt;</button>
+            </div>
+        </div>
         <Calendar
           localizer={localizer}
           events={[]}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 500, margin: '20px' }}
+          style={{ height: 500, width: "100%", margin: '20px', backgroundColor: "rgb(31 41 55)"}}
         />
       </Paper>
     </Container>
